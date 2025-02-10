b318d57152d8582e8a9a8264ef4613c0e8468d58
Lou Mauriege <lou.mauriege@laposte.net>
2025-02-11 00:34:37 +0100

    feat: setting up fullstack app

--- back/src/main/java/com/openclassrooms/mddapi/controller/TestController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TestController.java
@@ -1,4 +1,21 @@
 package com.openclassrooms.mddapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testGetEndpoint() {
+        return ResponseEntity.ok("\"message\": \"Get success\"");
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<String> testPostEndpoint() {
+        return ResponseEntity.ok("\"message\": \"Post success\"");
+    }
 }
