56d857ed8a1a38474ec1ba868be527a1e410b023
Lou Mauriege <lou.mauriege@laposte.net>
2025-02-14 21:56:32 +0100

    feat: graph scheme posts model

--- back/src/main/java/com/openclassrooms/mddapi/model/Post.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -9,15 +9,26 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "USERS")
-public class User {
+@Table(name = "POSTS")
+public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
-    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    private String title;
+
+//    @JoinTable(
+//            name = "POSTS_TOPICS",
+//            joinColumns = @JoinColumn(name = "post_id"),
+//            inverseJoinColumns = @JoinColumn(name = "topic_id"))
+//    private String topic;
+
+    private String content;
 
     @CreatedDate
     @Column(name = "created_at", updatable = false)
